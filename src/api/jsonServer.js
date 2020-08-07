13aa1309c07c0ba9670c97a94c23225d936e0b3d
danielcantillo0 <danielcantillo8@gmail.com>
2020-08-07 16:55:35 -0500

    Added comments

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://9a798c351afc.ngrok.io",
+  baseURL: "http://9a798c351afc.ngrok.io", //change this everytime you change the app
 });
